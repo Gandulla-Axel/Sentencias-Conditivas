e5e5b949b16e66c6d5aee0056ce01c2dc613fafc
axel Name <gandullaaxel727.gmail.com>
2022-07-13 09:15:05 -0300

    condicionales3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,64 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i <7 ; i++) {
+            if (i==0) {
+                System.out.println("*");
+            }else if (i==1){
+                System.out.println("**");
+            }else if (i==2){
+                System.out.println("***");
+            }else if (i==3){
+                System.out.println("****");
+            }else if (i==4){
+                System.out.println("*****");
+            }else if (i==5){
+                System.out.println("******");
+            }else if (i==6){
+                System.out.println("*******");
+            }
+
+        }
+        System.out.println();
+
+        for (int i = 0; i <7 ; i++) {
+            if (i==0) {
+                System.out.println("*******");
+            }else if (i==1){
+                System.out.println("******");
+            }else if (i==2){
+                System.out.println("*****");
+            }else if (i==3){
+                System.out.println("****");
+            }else if (i==4){
+                System.out.println("***");
+            }else if (i==5){
+                System.out.println("**");
+            }else if (i==6){
+                System.out.println("*");
+            }
+
+        }
+        System.out.println();
+        for (int i = 0; i <7 ; i++) {
+            if (i==0) {
+                System.out.println("                      *");
+            }else if (i==1){
+                System.out.println("                     **");
+            }else if (i==2){
+                System.out.println("                    ***");
+            }else if (i==3){
+                System.out.println("                   ****");
+            }else if (i==4){
+                System.out.println("                  *****");
+            }else if (i==5){
+                System.out.println("                 ******");
+            }else if (i==6){
+                System.out.println("                *******");
+            }
+
+        }
+
+
     }
+
 }
