4c1d310ac15d1a3dab74814d6f2aea50ebf99c50
axel Name <gandullaaxel727.gmail.com>
2022-07-13 08:25:00 -0300

    condicionales1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Scanner(System.in);
+        System.out.println("Ingrese Numeros ");
+        Scanner Num1 = new Scanner(System.in);
+        int i = Num1.nextInt();
+        Scanner Num2 = new Scanner(System.in);
+        int j = Num2.nextInt();
+        if (i > j) {
+            System.out.println("" + i + " Es mayor que " + j);
+        } else if (j > i) {
+            System.out.println("" + j + " Es mayor que " + i);
+        }
     }
 }
